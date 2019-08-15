870a3cd183bccac36e3a661fd71357645826dcdb
Anson <anson.liang@hotmail.com>
2019-08-15 17:03:57 -0600

    Added music, C# and finished the design of the game display.

---
+++ Music App/Assets/Scripts/UISystem.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISystem : MonoBehaviour
+{
+
+    UIPage PreviousPage;
+    UIPage CurrentPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIPage[] pages = GetComponentsInChildren<UIPage>(true);
+        pages[0].gameObject.SetActive(true);
+        for (int i = 1; i < pages.Length; i++)
+        {
+            pages[i].gameObject.SetActive(false);
+        }
+        CurrentPage = pages[0];
+
+
+    }
+
+
+    public void SwitchPage(UIPage nextPage)
+    {
+
+        //TODO: Disable the current page then enable the next page which is 'page'
+        PreviousPage = CurrentPage;
+        CurrentPage.gameObject.SetActive(false); // disable the gameObject that has the UIPage Compponent
+        nextPage.gameObject.SetActive(true);
+        CurrentPage = nextPage;
+
+    }
+
+    public void GoToPreviouspage()
+    {
+        SwitchPage(PreviousPage);
+    }
+
+
+
+
+
+
+}
