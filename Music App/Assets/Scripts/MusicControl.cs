95821a8b9f97bf19867a56037d56d71cb227ff48
Anson <anson.liang@hotmail.com>
2019-09-16 12:47:57 -0600

    final version

---
+++ Music App/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour
+{
+
+    public AudioSource source
+    { get
+        { return 
+                
+                GetComponent<AudioSource>();
+        }
+    }
+    public void PlayPauseSong()
+    {
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+        else
+        {
+            source.Pause();
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
